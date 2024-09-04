05d1ab40edab823f34e64abb8f624fd545bc7d59
Lina Jones <lina.jones630@gmail.com>
2024-09-03 18:57:18 -0600

    feat - add dog

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const catImage = new Image();
 catImage.src = 'assets/cat.png';
+
+const dogImage = new Image();
+dogImage.src = 'assets/dog.png';
  
 const canvas = document.getElementById('myCanvas') 
 const ctx = canvas.getContext('2d');
@@ -8,11 +11,12 @@ const ctx = canvas.getContext('2d');
 
  canvas.width = window.innerWidth;
  // 16:9 aspect ratio
- canvas.height = (9*window.innerHeight)/16;
+ canvas.height = (9 *window.innerHeight)/16;
 
 
  const drawImage = () => {
     ctx.drawImage(catImage, 10, 10, 100, 100);
+    ctx.drawImage(dogImage, 10, 300, 100, 100);
  }
 
 window.onload = drawImage;
